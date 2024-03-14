e5c695d74273a6c77aa44e79cc5d0e1dd1ddf8f7
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-03-13 21:35:45 -0700

    complete lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size):
+        self.brand = brand
+        # Avoid using same name for the property and the internal attribute to prevent confusion
+        # _size is the internal attribute and size is the property
+        self._size = size
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
